64277af7ed52e1d02d787b10b99a585e1c276e55
Keisuke SATO <riaf@me.com>
2012-09-12 10:29:51 +0900

    autoload support
    TODO:
        * API
        * Upload

---
+++ lib/Cloudinary/Cloudinary.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Cloudinary.php
+ *
+ * @author Keisuke SATO <sato@crocos.co.jp>
+ * @package cloudinary
+ * @license MIT License
+ */
+
+namespace Cloudinary;
+
+/**
+ * Cloudinary.
+ *
+ * @author Keisuke SATO <sato@crocos.co.jp>
+ */
+class Cloudinary
+{
+    /**
+     * @const VERSION
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * @const SHARED_CDN
+     */
+    const SHARED_CDN = "d3jpl91pxevbkh.cloudfront.net";
+}
